a735833f4aa7051e2bfeb071dada9497131ae30c
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-08-26 04:47:26 +0000

    1st commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const cors = require('cors');
+const sqlite3 = require('sqlite3');
+
+const app = express();
+const port = 3064;
+
+app.use(cors());
+app.use(express.json());
+
+const db = new sqlite3.Database('keyvaluestore.db');
+
+// Define your API endpoints
+app.post('/set', (req, res) => {
+  const { key, value } = req.body;
+  db.run('INSERT OR REPLACE INTO kvstore (key, value) VALUES (?, ?)', [key, value], (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error' });
+    } else {
+      res.json({ message: 'Key-value pair set successfully' });
+    }
+  });
+});
+
+app.get('/get/:key', (req, res) => {
+  const { key } = req.params;
+  db.get('SELECT value FROM kvstore WHERE key = ?', [key], (err, row) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error' });
+    } else if (row) {
+      res.json({ value: row.value });
+    } else {
+      res.status(404).json({ error: 'Key not found' });
+    }
+  });
+});
+
+app.get('/keys', (req, res) => {
+  db.all('SELECT key FROM kvstore', (err, rows) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error' });
+    } else {
+      const keys = rows.map(row => row.key);
+      res.json({ keys });
+    }
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
