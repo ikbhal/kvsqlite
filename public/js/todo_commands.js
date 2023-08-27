49490627823f0e55c5e15ee02d774caaa69945d9
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-08-27 07:56:51 +0000

    todo help, store

--- public/js/todo_commands.js
+++ public/js/todo_commands.js
@@ -1,34 +1,55 @@
+// Import the KVStore class from kvstore.js
+// import KVStore from './kvstore.js';
+
 class TodoList {
-  constructor() {
+  constructor(kvStore) {
     this.todos = [];
+    this.kvStore = kvStore;
   }
 
-  handleCommand(command, args) {
+  async handleCommand(command, args) {
     switch (command) {
       case 'add':
         return this.handleAddTodo(args);
       case 'list':
         return this.handleListTodos();
-      // Implement other cases...
       case 'completed':
         return this.handleMarkCompleted(args, true);
       case 'uncompleted':
         return this.handleMarkCompleted(args, false);
       case 'delete':
         return this.handleDeleteTodo(args);
-
+      case 'help':
+        return this.showHelp();
       default:
         return 'Unknown command.';
     }
   }
 
-  handleAddTodo(args) {
+  showHelp() {
+    const helpMessage = [
+      '/todo add <text> - Add a new todo.',
+      '/todo list - List all todos.',
+      '/todo completed <index> - Mark a todo as completed.',
+      '/todo uncompleted <index> - Mark a todo as uncompleted.',
+      '/todo delete <index> - Delete a todo.',
+      '/todo help - Show this help message.'
+    ].join('<br>');
+
+    return helpMessage;
+  }
+
+  async handleAddTodo(args) {
     const todoText = args.join(' ');
     this.todos.push({ text: todoText, completed: false });
+
+    // Store todos in KVStore
+    await this.kvStore.setKeyValue('todos', JSON.stringify(this.todos));
+
     return `Todo added: ${todoText}`;
   }
 
-  handleListTodos() {
+  async handleListTodos() {
     if (this.todos.length === 0) {
       return 'No todos available.';
     } else {
@@ -40,29 +61,40 @@ class TodoList {
     }
   }
 
-  handleMarkCompleted(args, completed) {
+  async handleMarkCompleted(args, completed) {
     const index = parseInt(args[0]) - 1;
     if (isNaN(index) || index < 0 || index >= this.todos.length) {
       return 'Invalid todo index.';
     }
 
     this.todos[index].completed = completed;
+
+    // Update and store todos in KVStore
+    await this.kvStore.setKeyValue('todos', JSON.stringify(this.todos));
+
     const status = completed ? 'completed' : 'uncompleted';
     return `Todo ${index + 1} marked as ${status}.`;
   }
 
-  handleDeleteTodo(args) {
+  async handleDeleteTodo(args) {
     const index = parseInt(args[0]) - 1;
     if (isNaN(index) || index < 0 || index >= this.todos.length) {
       return 'Invalid todo index.';
     }
 
     const deletedTodo = this.todos.splice(index, 1)[0];
+
+    // Update and store todos in KVStore
+    await this.kvStore.setKeyValue('todos', JSON.stringify(this.todos));
+
     return `Todo ${index + 1} deleted: ${deletedTodo.text}`;
   }
 
-  // Implement other methods for handling todo commands...
+  // ... other methods ...
 }
 
-  // Export the class
-//   export default TodoCommands;
+// Instantiate the KVStore class
+// const kvStore = new KVStore();
+
+// Instantiate the TodoList class with the KVStore instance
+const todoList = new TodoList(kvStoreInstance);
