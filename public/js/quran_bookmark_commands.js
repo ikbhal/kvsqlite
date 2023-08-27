eb6ccf47a8be02013f7eb6a51793951f05bb5962
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-08-27 07:21:04 +0000

    store quran

--- public/js/quran_bookmark_commands.js
+++ public/js/quran_bookmark_commands.js
@@ -13,80 +13,98 @@ const quranSurahAyatsArray = [
   5, 8, 8, 11, 11, 8, 3, 9, 5, 4, 7, 3, 6, 3,
   5, 4, 5, 6
 ];
-
-
-class QuranCommands {
-  constructor() {
-    this.surah = 1;
-    this.ayat = 1;
+class QuranBookmarkCommands {
+  constructor(kvStore) {
+    this.kvStore = kvStore;
   }
 
   handleCommand(command, args) {
     switch (command) {
       case 'start':
-        return this.handleStartQuran(args);
+        return this.handleStartBookmark();
       case 'status':
-        return this.handleStatusQuran();
+        return this.handleStatusBookmark();
       case 'reset':
-        return this.handleResetQuran();
+        return this.handleResetBookmark();
       case 'next':
-        return this.handleNextQuran();
+        return this.handleNextBookmark();
       case 'help':
         return this.showHelp();
       default:
         return 'Unknown command.';
     }
   }
-  
+
   showHelp() {
     const helpMessage = [
-      '/quran start - Start reading the Quran from a specific ayah.',
-      '/quran status - Show the current reading status.',
-      '/quran reset - Reset the reading back to the beginning.',
-      '/quran next - Move to the next ayah.',
+      '/quran start - Start reading the Quran from a specific bookmarked ayah.',
+      '/quran status - Show the current bookmarked position.',
+      '/quran reset - Reset the bookmark to the beginning.',
+      '/quran next - Move to the next bookmarked ayah.',
       '/quran help - Show this help message.'
     ].join('<br>');
-  
+
     return helpMessage;
   }
-  
-  handleStartQuran(args) {
-    this.surah = 1;
-    this.ayat = 1;
-    const quranUrl = `https://quran.com/${this.surah}/${this.ayat}`;
-    return `Quran starting position set. <br/> <a href="${quranUrl}" target="_blank">Open in Quran.com</a><br/>`;
-  }
 
-  handleStatusQuran() {
-    const quranLink = `https://quran.com/${this.surah}/${this.ayat}`;
-    return `Current position: Surah ${this.surah}, Ayat ${this.ayat}. [${quranLink}] <br/><a href="${quranLink}" target="_blank">Open in Quran.com</a><br/>`;
-  }
-
-  handleResetQuran() {
-    this.surah = 1;
-    this.ayat = 1;
-    return 'Quran position reset to Surah 1, Ayat 1.';
-  }
-
-  handleNextQuran() {
-    const totalAyats = quranSurahAyatsArray[this.surah - 1];
-
-    if (this.ayat < totalAyats) {
-      this.ayat++;
-    } else if (this.ayat >= totalAyats) {
-      this.surah++;
-      this.ayat = 1;
-      if (this.surah >= quranSurahAyatsArray.length) {
-        this.surah = 1;
-      }
-    } else {
-      return 'You have reached the end of the Quran.';
+  async handleStartBookmark() {
+    try {
+      const defaultValue = { surah: 1, ayat: 1 };
+      await this.kvStore.setKeyValue('quranBookmark', JSON.stringify(defaultValue));
+      const quranUrl = `https://quran.com/${defaultValue.surah}/${defaultValue.ayat}`;
+      return `Quran bookmark position set to default. <br/> <a href="${quranUrl}" target="_blank">Open in Quran.com</a><br/>`;
+    } catch (error) {
+      return `Failed to set Quran bookmark: ${error.message}`;
     }
+  }
 
-    const quranLink = `https://quran.com/${this.surah}/${this.ayat}`;
-    return `Moved to next ayat: Surah ${this.surah}, Ayat ${this.ayat}. [${quranLink}] <br/> <a href="${quranLink}" target="_blank">Open in Quran.com</a><br/>`;
+
+  async handleStatusBookmark() {
+    try {
+      const bookmark = await this.kvStore.getKeyValue('quranBookmark');
+      if (bookmark) {
+        const { surah, ayat } = JSON.parse(bookmark);
+        const quranLink = `https://quran.com/${surah}/${ayat}`;
+        return `Current bookmarked position: Surah ${surah}, Ayat ${ayat}. [${quranLink}] <br/><a href="${quranLink}" target="_blank">Open in Quran.com</a><br/>`;
+      } else {
+        return 'No Quran bookmark found.';
+      }
+    } catch (error) {
+      return `Failed to retrieve Quran bookmark: ${error.message}`;
+    }
+  }
+
+  async handleResetBookmark() {
+    try {
+      await this.kvStore.deleteKey('quranBookmark');
+      return 'Quran bookmark reset.';
+    } catch (error) {
+      return `Failed to reset Quran bookmark: ${error.message}`;
+    }
+  }
+
+  async handleNextBookmark() {
+    try {
+      const bookmark = await this.kvStore.getKeyValue('quranBookmark');
+      if (bookmark) {
+        const { surah, ayat } = JSON.parse(bookmark);
+        const totalAyats = quranSurahAyatsArray[surah - 1];
+        if (ayat < totalAyats) {
+          const newBookmark = JSON.stringify({ surah, ayat: ayat + 1 });
+          await this.kvStore.setKeyValue('quranBookmark', newBookmark);
+          const quranLink = `https://quran.com/${surah}/${ayat + 1}`;
+          return `Moved to next bookmarked ayah: Surah ${surah}, Ayat ${ayat + 1}. [${quranLink}] <br/><a href="${quranLink}" target="_blank">Open in Quran.com</a><br/>`;
+        } else {
+          return 'You have reached the end of the bookmarked surah.';
+        }
+      } else {
+        return 'No Quran bookmark found. Use /quran start to set a new bookmark.';
+      }
+    } catch (error) {
+      return `Failed to update Quran bookmark: ${error.message}`;
+    }
   }
 }
 
-// Instantiate the QuranCommands class with your kvstore instance
-const quranCommands = new QuranCommands();
+// Instantiate the QuranBookmarkCommands class with the kvStoreInstance
+const quranBookmarkCommands = new QuranBookmarkCommands(kvStoreInstance);
